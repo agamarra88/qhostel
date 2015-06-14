dba888c866c089ed64277b0558465250885d6da2
lianrey <lianareyesx@gmail.com>
2015-06-13 23:06:18 -0500

    add routes

---
+++ admin/Scripts/routes.js
@@ -0,0 +1,48 @@
+﻿myApp.config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise("/home");
+
+    $stateProvider
+        .state('home', {
+            url: "/home",
+            templateUrl: "Templates/home.html",
+            controller: function ($scope) {
+                $scope.items = ["A", "List", "Of", "Items"];
+            }
+        })
+        .state('nosotros', {
+            url: "/nosotros",
+            templateUrl: "Templates/nosotros.html",
+            controller: function ($scope) {
+                $scope.items = ["A", "List", "Of", "Items"];
+            }
+        })
+        .state('tarifas', {
+            url: "/tarifas",
+            templateUrl: "Templates/tarifas.html",
+            controller: function ($scope) {
+                $scope.items = ["A", "List", "Of", "Items"];
+            }
+        })
+        .state('promociones', {
+            url: "/promociones",
+            templateUrl: "Templates/promociones.html",
+            controller: function ($scope) {
+                $scope.items = ["A", "List", "Of", "Items"];
+            }
+        })
+        .state('bar', {
+            url: "/bar",
+            templateUrl: "Templates/bar.html",
+            controller: function ($scope) {
+                $scope.items = ["A", "List", "Of", "Items"];
+            }
+        })
+        .state('contacto', {
+            url: "/contacto",
+            templateUrl: "Templates/contacto.html",
+            controller: function ($scope) {
+                $scope.items = ["A", "List", "Of", "Items"];
+            }
+        })
+});
